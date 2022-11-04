a5fa409240027ba94b6babad3215a712f93aa3af
Diego Paz <diegoparancib@gmail.com>
2022-11-04 16:44:43 -0300

    Add metas

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Footer from 'components/Footer'
 import Hero from 'components/Hero'
 import MainLayout from 'components/Layouts/MainLayout'
+import Metas from 'components/Seo/Metas'
 import Services from 'components/Services'
 import Sucursales from 'components/Sucursales'
 import Why from 'components/Why'
@@ -8,6 +9,10 @@ import Why from 'components/Why'
 export default function Home() {
   return (
     <MainLayout>
+      <Metas
+        title="Expreso TAS | Transporte y Distribución"
+        description="Expreso TAS transporta y distribuye encomiendas de largas distancias en la provincia de Buenos Aires y Rosario en 24hs"
+      />
       <Hero />
       <Why />
       <Services />
